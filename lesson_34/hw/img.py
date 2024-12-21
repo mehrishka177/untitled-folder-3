bc56dcd39e1034ab62f3ca5c7d8a64bb3bcd2914
mehrishka177 <mehri@hajiagali.com>
2024-12-21 10:42:12 +0400

    kdsfb

---
+++ lesson_34/hw/img.py
@@ -0,0 +1,10 @@
+import pygame
+import random
+
+SCREEN_HIGHT = (500, 500)
+SCREEN_WIDTH = (400, 400)
+BACKGROUND_COLOR = GREY (58, 58, 58)
+
+IMAGE_SIZE = (300, 300)
+IMPORT_IMAGE_LINK = (https://upload.wikimedia.org/wikipedia/commons/e/ea/Mp3.svg)
+
